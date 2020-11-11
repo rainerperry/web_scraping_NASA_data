12cd3df862419c0562c88d5d0f8ddd187d6cbdfd
rainerperry <68960541+rainerperry@users.noreply.github.com>
2020-11-11 12:36:41 -0500

    finished jupyter notbeook and html

---
+++ scrape_mars.py
@@ -0,0 +1,60 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape_info():
+    browser = init_browser()
+
+    # URL OF NASA website to be scraped
+    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    browser.visit(url)
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+
+    # find the relevant class items for the title and scrape it
+    step1 = soup.find('ul', class_='item_list')
+    results = step1.find('li', class_="slide")
+    title = results.find('div', class_="content_title").text
+
+    #find the relevatn class items for paragraph and scrape them
+    paragraph = results.find('div', class_="article_teaser_body").text
+
+
+
+    # Get the average temps
+    avg_temps = soup.find('div', id='weather')
+
+    # Get the min avg temp
+    min_temp = avg_temps.find_all('strong')[0].text
+
+    # Get the max avg temp
+    max_temp = avg_temps.find_all('strong')[1].text
+
+    # BONUS: Find the src for the sloth image
+    relative_image_path = soup.find_all('img')[2]["src"]
+    sloth_img = url + relative_image_path
+
+    # Store data in a dictionary
+    costa_data = {
+        "sloth_img": sloth_img,
+        "min_temp": min_temp,
+        "max_temp": max_temp
+    }
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # Return results
+    return costa_data
